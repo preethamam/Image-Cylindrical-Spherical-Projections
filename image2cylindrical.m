a153d57310d87f289810ed65bc2f0ed9cd12e847
Preetham Manjunatha <preethamam@gmail.com>
2021-08-02 10:04:46 -0700

    Removed forloop

--- image2cylindrical.m
+++ image2cylindrical.m
@@ -55,13 +55,15 @@ ximg = floor(f * xd + xc);
 yimg = floor(f * yd + yc);
 
 % Find boundary of the cylindrical projection
-[row,col] = find(ximg > 0 & ximg <= xdim & yimg > 0 & yimg <= ydim);
+region = (ximg > 0 & ximg <= xdim & yimg > 0 & yimg <= ydim);
 
-% Cylindrical warped image
-% imageCylindrical(row,col,:) = image(row,col,:);
-for i=1:length(row)    
-   imageCylindrical(row(i),col(i),:) = image(row(i),col(i),:); 
-end
+% Temporary variables
+imtmp  = reshape(image,[], size(image,3));
+outtmp = reshape(imageCylindrical, [], size(imageCylindrical,3));
+outtmp(region,:) = imtmp(region,:);
+
+% Get projections
+imageCylindrical(:) = outtmp(:);
 
 end
 
